3778dcba219d07b099348875ead27537b7e2642c
Laura Nadolski <laura.nadolski@gmail.com>
2018-01-10 23:30:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // write your code below!
+
+function happyHolidays() {
+  return ["Happy holidays!"];
+}
+
+function happyHolidaysTo(name){
+  return [`Happy holidays, ${name}!`];
+}
+
+function happyHolidayTo(holiday, name) {
+  return [`Happy ${holiday}, ${name}!`];
+}
+
+function holidayCountdown(holiday, daysremaining){
+ if (daysremaining > 1){
+  return [`It's ${daysremaining} days until ${holiday}!`];
+   
+ }
+  
+else if (daysremaining === 1){
+  return [`It's ${daysremaining} day until ${holiday}!`];
+}
+}

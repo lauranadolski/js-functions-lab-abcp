5990ab9a95a04748d3986ad3ea400c631ea66b0e
Laura Nadolski <laura.nadolski@gmail.com>
2018-01-10 23:18:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function happyHolidays() {
   return ["Happy holidays!"];
 }
 
+function happyHolidaysTo(name){
+  return [`Happy Holidays, ${name}!`];
+}
+
 function happyHolidayTo(holiday, name) {
   return [`Happy ${holiday}, ${name}!`];
 }
